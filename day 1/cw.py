8ec3aabd437a40eec56437fef9edfa349a3ea5a4
Khimento <gociridze62@gmail.com>
2025-07-11 17:04:25 +0400

    1

---
+++ day 1/cw.py
@@ -0,0 +1,14 @@
+num1=10.5
+num2=35
+print (num1 + num2)
+print (num1 / num2)
+print (num1 * num2)
+print (num1 - num2)
+num3 = 50
+print(num1 * num2 + num3)
+age= 15
+print (age)
+age= 20
+print(age)
+number = 10
+print (number)
